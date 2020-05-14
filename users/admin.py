0479686cd12b915bef849727579db8e975e35774
Mahdi Bahari <baharimahdi93@gmail.com>
2020-05-15 02:30:26 +0430

    .

--- users/admin.py
+++ users/admin.py
@@ -16,7 +16,7 @@ class UserAdmin(DjangoUserAdmin):
     # }
     fieldsets = (
         (None, {'fields': ('mobile', 'password')}),
-        (_('Personal info'), {'fields': ('first_name', 'last_name','email', 'national_code', 'birth_date')}),
+        (_('Personal info'), {'fields': ('first_name', 'last_name','email', 'national_code', 'birth_date','file','trusted')}),
         (_('Permissions'), {'fields': ('is_active', 'is_staff', 'is_superuser',
                                        'groups', 'user_permissions')}),
         (_('Important dates'), {'fields': ('last_login', 'date_joined')}),
