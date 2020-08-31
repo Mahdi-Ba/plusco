a0a193b487ab3bf01e04f072d0b799cdc9409a26
Mahdi Bahari <baharimahdi93@gmail.com>
2020-08-31 11:40:30 +0430

    status

--- organizations/admin.py
+++ organizations/admin.py
@@ -9,15 +9,15 @@ class StatusAdmin(admin.ModelAdmin):
     list_display = ['title', ]
     search_fields = ['title']
 
-    # def has_add_permission(self, request):
-    #     return False
-    #
-    # def has_change_permission(self, request,obj=None):
-    #     return False
-    #
-    # def has_delete_permission(self, request,obj=None):
-    #     return False
-    #
+    def has_add_permission(self, request):
+        return False
+
+    def has_change_permission(self, request,obj=None):
+        return False
+
+    def has_delete_permission(self, request,obj=None):
+        return False
+
 
 
 
