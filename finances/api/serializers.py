edd73477b459928da7c038b674f4fd967ee720fa
Mahdi Bahari <baharimahdi93@gmail.com>
2020-09-30 23:40:50 +0330

    .

--- finances/api/serializers.py
+++ finances/api/serializers.py
@@ -2,6 +2,7 @@ from django.utils.text import slugify
 from rest_framework import serializers
 from rest_framework.validators import UniqueValidator, UniqueTogetherValidator
 
+from organizations.api.serializers import FactorySerilizer
 from users.api.serializers import BriefUser
 from ..models import *
 from django.urls import reverse
@@ -19,6 +20,22 @@ class PlanSerilizer(serializers.ModelSerializer):
         exclude = ('is_free',)
 
 class FactoryPlanSerilizer(serializers.ModelSerializer):
+    user = serializers.CharField(required=False)
+    user_detail = BriefUser(many=False, required=False, read_only=True, source='user')
+    factory = FactorySerilizer(required=False, many=False)
     class Meta:
         model = FactoryPlan
-        fields = '__all__'
+        fields = [
+            'factory',
+            'user',
+            'user_detail',
+            'count',
+            'start_date',
+            'end_date',
+            'price',
+            'percent',
+            'price_with_tax',
+            'is_success',
+            'ref_id',
+
+        ]
